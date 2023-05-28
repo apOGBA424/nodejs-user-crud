6a60c027f42ef98c120a2325cd183c2b3aa9022f
Paschal A. Ogba <paxbond01@gmail.com>
2023-05-28 05:22:53 +0100

    added protected route and checkAuthMiddleware

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+
 
 // middlewares
 const app = express();
@@ -12,8 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // variables
 const port = process.env.PORT || 3000;
-// const jwtsecret = process.env.JWT_SECRET;
-// const secretkey = process.env.SECRET_KEY;
+const jwtsecret = process.env.JWT_SECRET;
 
 
 // temporal database
@@ -24,18 +25,29 @@ app.listen(port, ()=>{console.log(`server running on port ${port}`)});
 
 
 
-//*******************
-//      ROUTES
-//******************* 
+//**************************************
+//            ROUTES
+//************************************** 
+
+
 // get all user registered in database
 app.get('/users', (req, res)=>{
     console.log('all users');
+    console.table(usersList);
     res.json(usersList);
 });
 
 
 // create a new user
 app.post('/signup', async(req, res)=>{
+
+    // check if the user is registered already
+    const alreadyUser = usersList.find((user)=>user.name === req.body.name);
+
+    if (alreadyUser) {
+        return res.status(409).json({'msg': 'User already exists'});
+    }
+
     const user = {
         user_id : new Date().valueOf().toString(),
         name : req.body.name,
@@ -47,25 +59,71 @@ app.post('/signup', async(req, res)=>{
 });
 
 
+
 // login
 app.post('/users/login', async(req, res)=>{
-    const user = usersList.find((user)=>user.name === req.body.name);
+    const userAccount = usersList.find((user)=>user.name === req.body.name);
     
-    if (user == null) {
-        res.status(400).send({'msg':'user not found'})
+    // check if user account exists
+    if (userAccount == null) {
+        res.status(400).send({'msg':'user not found'});
     }
 
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
+    try {  
+        // compare current password with hasdedPassword
+        const isPasswordValid = await bcrypt.compare(req.body.password, userAccount.password);
+        if (!isPasswordValid) {
+            res.status(400).send({'msg':'password is incorrect'});
+        }
+
+
+        // if password is correct, sign jwt accessToken
+        if (isPasswordValid) {
+            console.info(`user_id--> ${userAccount.user_id}`)
+            console.info(`jwtsecret--> ${jwtsecret}`)
+            
+            // generate an access accessToken
+            const accessToken = jwt.sign({user_id: userAccount}, jwtsecret) //jwtsecret is: 'jwt-secr3t'
+            
             console.info('log-in successful')
-            res.status(200).send({'msg':`${user.name} log-in successful`})
+           
+            // store the accessToken in a cookie
+            // res.cookie('accessToken', accessToken, {httpOnly: true});
+            const cookietoken = res.cookie('accessToken', accessToken, {httpOnly: true});
+
+            res.status(200).json({'msg':`${userAccount.name} log-in successful`,accessToken,jwtsecret});
         }else{
             res.status(400).send({'msg':'log-in failed'})
         }
     } catch (err) {
-        res.status(500).send();
+        res.status(500).json({'msg': err.message});
     }
 });
+
+
+// create auth bearer token middleware
+function checkAuthMiddleware(req, res, next) {
+    const accessToken = req.header.authorization;
+
+    let isTest = true;
+    // let isTest = false;
+    if (isTest) {
+        console.log('\n\n\nvalid credentials');
+        // res.json({'msg':'authorized',usersList,accessToken})
+        res.redirect('/users')
+    }else{
+        res.status(401).json({'msg':'invalid credentials'})
+    }
+
+    next();
+}
+
+
+// protected routes only for authorised users
+app.get('/user/protected',checkAuthMiddleware ,(req, res)=>{
+    // res.json({'msg':'protected-route middleware'});
+});
+
 
 
 // get one user by id
@@ -97,7 +155,7 @@ app.put('/user/update/:id', (req, res)=>{
 app.get('/user/logout/:id', (req, res)=>{
     const user = usersList.find((user)=>user.user_id === req.params.id);
     console.log(user)
-    res.status(200).json({msg: 'log-out successful'});
+    res.status(204).json({msg: 'log-out successful'});
 });
 
 
@@ -110,8 +168,9 @@ app.get('/user/delete/:id', (req, res)=>{
         res.status(404).send({'msg': 'user account not found'});
     }
 
-    // remove user form userList
+    // remove user from userList
     const deletedUser = usersList.splice(userIndex, 1)[0];
     
     res.status(200).json({msg: `${deletedUser['name']} acct delete successful`});
 });
+
